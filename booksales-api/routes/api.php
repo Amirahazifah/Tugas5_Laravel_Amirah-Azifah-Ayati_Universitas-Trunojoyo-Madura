f2c5779f2e679c5bf25b8931ad6d1d9789f496e6
Amirah Azifah Ayati <113626717+Amirahazifah@users.noreply.github.com>
2025-05-24 00:22:36 +0700

    Update api.php

--- booksales-api/routes/api.php
+++ booksales-api/routes/api.php
@@ -6,12 +6,10 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\AuthorsController;
 
-// Route user login (auth:sanctum)
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// API Resources
 Route::apiResource('books', BookController::class);
 Route::apiResource('genres', GenreController::class);
 Route::apiResource('authors', AuthorsController::class);
